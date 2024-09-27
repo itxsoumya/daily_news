90c8c98562ce892a4673907d62b13f49afd1b260
itxsoumya <itxsoumyaranjan@gmail.com>
2024-09-27 06:50:39 +0530

    state

---
+++ frontend/src/state/useAuthStore.js
@@ -0,0 +1,13 @@
+import create from 'zustand';
+
+const useAuthStore = create((set) => ({
+    user: null,
+    token: null,
+    loading: true,
+    setUser: (user) => set({ user }),
+    setToken: (token) => set({ token }),
+    setLoading: (loading) => set({ loading }),
+    logout: () => set({ user: null, token: null }),
+}));
+
+export default useAuthStore;
