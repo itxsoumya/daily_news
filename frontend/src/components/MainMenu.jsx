f1996255657e319bade2040e0c920c34298b104e
itxsoumya <itxsoumyaranjan@gmail.com>
2024-10-14 19:18:34 +0530

    update

--- frontend/src/components/MainMenu.jsx
+++ frontend/src/components/MainMenu.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import Category from "./Category";
+import useAuthStore from "../state/useAuthStore";
 
 const MainMenu = () => {
+  const AuthUser = useAuthStore((state) => state.user);
   console.log("menuuuu");
+  const navigate = useNavigate();
   return (
     <dialog id="my_modal_2" className="modal">
       <div className="modal-box bg-red-300x h-full">
@@ -18,53 +22,60 @@ const MainMenu = () => {
         </div>
 
         <div className="flex flex-col bg-cyan-300x gap-1">
-          <div className="avatar placeholder bg-red-400x mx-auto">
-            <div className="bg-neutral text-neutral-content w-24 rounded-full">
-              <span className="text-3xl">D</span>
+          {AuthUser ? (
+            <>
+              <div className="avatar placeholder bg-red-400x mx-auto">
+                <div className="bg-neutral text-neutral-content w-24 rounded-full">
+                  <span className="text-3xl">D</span>
+                </div>
+              </div>
+              <div className="text-2xl  text-center break-words">John Doe</div>
+              <div className="flex gap-2 mt-4 ">
+                <button className="btn w-1/2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                  Liked Post
+                </button>
+                <div className="btn w-1/2" onClick={() => navigate("/logout")}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+                    />
+                  </svg>
+                  Logout
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="flex gap-3">
+              <button className="btn btn-neutral grow" onClick={()=>navigate('/signup')}>Sign Up</button>
+              <button className="btn grow" onClick={()=>navigate('/signin')}>Sign In</button>
             </div>
-          </div>
-          <div className="text-2xl  text-center break-words">John Doe</div>
-          <div className="flex gap-2 mt-4 ">
-            <button className="btn w-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              Liked Post
-            </button>
-            <div className="btn w-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
-                />
-              </svg>
-              Logout
-            </div>
-          </div>
+          )}
         </div>
 
-        <Category/>
-    
-
+        <Category />
       </div>
     </dialog>
   );
--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -86,26 +86,11 @@ const Signin = () => {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-800"
-                  >
-                    Remember me
-                  </label>
-                </div>
-              </div>
+              
               <div>
                 <button
                   type="submit"
-                  className="btn w-full btn-neutral"
+                  className="btn w-full btn-neutral mt-6"
                   disabled={loading}
                 >
                   {loading ? (
